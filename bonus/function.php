f3b1fc0a37b734824126a672604f9c3ae29e12f0
Luigi213 <reyesgianluigi88@gmail.com>
2023-01-31 16:23:41 +0100

    optimezed rand

--- bonus/function.php
+++ bonus/function.php
@@ -1,6 +1,6 @@
 <?php
     function rand_string( $length ) {
-        $pass = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        $pass = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|':\"<>,.?\/";
         return substr(str_shuffle($pass),0,$length);        
     }        
 ?>
